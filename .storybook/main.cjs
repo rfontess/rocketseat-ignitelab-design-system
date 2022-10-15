80e715aa12358594dcdbccb0114a95af91bef5b4
Roberto Fontes <robertofontes2@gmail.com>
2022-10-14 21:19:09 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/rocketseat-ignitelab-design-system/'
+    }
+
+    return config
   }
 }
